79e03db5e39b1ae60d83d7a008ae31a25aa5c534
alexandrmakarevich <lemas096@gmail.com>
2021-07-05 11:11:58 +0300

    feat: comb, tel complete

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,9 +1,27 @@
 package subtask2
 
+import kotlin.math.sqrt
+
 class SquareDecomposer {
 
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        // 50 {1, 3, 5, 8, 49}
+        val squareArray: ArrayList<Int> = arrayListOf()
+        for (i in number*number-1 downTo 1 step 1){
+            if (isWhole(sqrt(i.toDouble()))){
+                squareArray.add(i)
+            }
+        }
+        squareArray.forEach { println(it) }
+
+
+
+        return null
     }
+
+    private fun isWhole(d: Double): Boolean{
+        return d - d.toInt() == 0.0
+    }
+
 }
